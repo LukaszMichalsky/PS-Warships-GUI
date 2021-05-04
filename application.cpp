bd325fb4051e2ea8ebf7f6409d10f13470181dcf
Jakub Wyciślik <jakuwyc150@gmail.com>
2021-05-04 09:35:23 +0200

    Added start server page

--- application.cpp
+++ application.cpp
@@ -2,7 +2,8 @@
 #include "ui_application.h"
 
 Application::Application(QWidget *parent) : QMainWindow(parent), ui(new Ui::Application) {
-  ui->setupUi(this);
+  ui -> setupUi(this);
+  ui -> pagesWidget -> setCurrentIndex(0);
 }
 
 Application::~Application() {
